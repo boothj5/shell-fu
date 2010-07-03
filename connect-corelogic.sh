0408bc018538179f2212e659bc1b9efaa775da24
James Booth <boothj5@gmail.com>
2010-07-03 19:21:07 +0100

    Removed password and added vpn connect script

---
+++ connect-corelogic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# connect to vpn
+sudo vpnc --enable-1des corelogic.conf
+
+# route to servers
+sudo route add -net 192.168.0.29 netmask 255.255.255.255 tun0 # hudson.corelogic.local
+sudo route add -net 192.168.0.90 netmask 255.255.255.255 tun0 # james-pc.corelogic.local
+sudo route add -net 192.168.0.10 netmask 255.255.255.255 tun0 # svn.corelogic.local
+sudo route add -net 192.168.0.26 netmask 255.255.255.255 tun0 # framework2.corelogic.local
+
+# mount filesystems
+sudo mount -t cifs -o username=james.booth,password=<password>,uid=james //192.168.0.26/Share ~/mnt/framework2/
+
