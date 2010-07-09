d08630ff12b098d407713d4d408622f74ad9b00f
james.booth <jamesbooth2000@hotmail.com>
2010-07-09 01:15:18 +0100

    Added build-info script

---
+++ build-tools/build-info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Parameter one is the projects base folder (where all the components source is)
+# e.g. /home/james/projects
+
+components=`ls -1 $1 | grep corelogic`
+
+echo Components:
+echo $components
+echo
+
+for comp in $components; do
+	echo Component: 
+	printf "\t%s\n" $comp
+        rev=`cat $1/$comp/build.properties | grep module.revision | sed 's/module.revision=\([0-9]\..*$\)/\1/g'`
+	printf "\tRevision: %s\n" $rev
+	printf "Depends on :\n"
+	cat $1/$comp/dependencies.properties | grep ^corelogic | grep -v branch | sed 's/\(^corelogic.*\)\.rev=\(.*\)/\t\1: \2/g'
+	echo
+done
