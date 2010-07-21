7d7362b3a2939d1d5d83990c4195fd81aafbcbd8
james <james@jamesbuntu.(none)>
2010-07-21 18:19:59 +0100

    Fixed some stuped bugs after testing
    Mistyped path names and forgot "." so was looking in /

--- git-svn-helpers/create-RELEASE.sh
+++ git-svn-helpers/create-RELEASE.sh
@@ -13,11 +13,11 @@ mkdir $base_folder
 for component in $components; do
 
 	# clone each component into it
-	git svn clone  $svn_url --trunk=trunk/$component --branches=branches/$component --prefix=svn/ c./$base_folder/$component
+	git svn clone  $svn_url --trunk=trunk/$component --branches=branches/$component --prefix=svn/ ./$base_folder/$component
 
 	# reset to HEAD to master as git by default will point it to the most recent commit 
 	# of all in the tree
-	cd /$base_folder/$component
+	cd ./$base_folder/$component
 	git reset --hard remotes/svn/trunk
 
 	# got back to where we were
