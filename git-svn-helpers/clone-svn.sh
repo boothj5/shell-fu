de2181da09d0e157c8e09129cba6c6b6aa8de6de
james <james@jamesbuntu.(none)>
2010-07-22 16:51:30 +0100

    Added usage of base path
    The folder under which the projects folder will be created is now
    specified and used.
    
    Also remotes to the public repos for each private repo

--- git-svn-helpers/clone-svn.sh
+++ git-svn-helpers/clone-svn.sh
@@ -3,24 +3,25 @@
 # assume this is run in the directory where the projects-git-RELEASE folder will be created
 # assume user has authenticated with SVN already
 
-components="corelogic-checkserializable corelogic-common corelogic-fileloader corelogic-frameworki corelogic-frameworki-codegen corelogic-frameworki-mobile corelogic-frameworki-xb corelogic-interfaceserver corelogic-interfaceserver-xb corelogic-jobs corelogic-jobs-admin-tool corelogic-logging corelogic-master-build corelogic-portlet corelogic-test-framework"
-svn_url=http://svn.corelogic.local/svn/main
-base_folder=projects-git-RELEASE
+base_path=/home/james
+components="corelogic-checkserializable corelogic-common corelogic-fileloader corelogic-frameworki-codegen corelogic-frameworki-mobile corelogic-frameworki-xb corelogic-interfaceserver corelogic-interfaceserver-xb corelogic-jobs corelogic-jobs-admin-tool corelogic-logging corelogic-portlet corelogic-test-framework"
+svn_url=http://localhost/svn/main
+projects_folder=projects-git-RELEASE
 
 # create the base folder
-mkdir $base_folder
+mkdir $base_path/$projects_folder
 
 for component in $components; do
 
 	# clone each component into it
-	git svn clone  $svn_url --trunk=trunk/$component --branches=branches/$component --prefix=svn/ ./$base_folder/$component
+	git svn clone  $svn_url --trunk=trunk/$component --branches=branches/$component --prefix=svn/ $base_path/$projects_folder/$component
 
 	# reset to HEAD to master as git by default will point it to the most recent commit 
 	# of all in the tree
-	cd ./$base_folder/$component
+	cd $base_path/$projects_folder/$component
 	git reset --hard remotes/svn/trunk
 
 	# got back to where we were
-	cd ../../
+	cd $base_path
 done
 
--- git-svn-helpers/make-public.sh
+++ git-svn-helpers/make-public.sh
@@ -2,28 +2,32 @@
 
 # assume this is run in the directory where the projects-git-RELEASE folder is.
 
+base_path=/home/james
 private=projects-git-RELEASE
 public=projects-git-RELEASE-pub
-components=`ls -1 $private`
+components=`ls -1 $base_path/$private`
 
-mkdir $public
+mkdir $base_path/$public
 
 for component in $components; do
 
 	# create and initalise the public repo
-	mkdir ./$public/$component.git
-	cd ./$public/$component.git
+	mkdir $base_path/$public/$component.git
+	cd $base_path/$public/$component.git
 	git init --bare
 	# make it visible to git-daemon for sharing
 	touch git-daemon-export-ok
 
 	# push the content from private to public
-	cd ../../$private/$component
-	git push --all ../../$public/$component.git
-	git push ../../$public/$component.git 'refs/remotes/svn/*:refs/heads/svn/*'
+	cd $base_path/$private/$component
+	git push --all $base_path/$public/$component.git
+	git push $base_path/$public/$component.git 'refs/remotes/svn/*:refs/heads/svn/*'
+
+	# set up a remote to the public repository so we can push to it
+	git remote add public file:///$base_path/$public/$component.git
 
 	# go back to base
-	cd ../../
+	cd $base_path
 done
 
 
