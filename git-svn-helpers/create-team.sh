94b0388badd2a01f91d789aee6818d11e1efe290
james <james@jamesbuntu.(none)>
2010-07-22 18:07:29 +0100

    create-team.sh now works

--- git-svn-helpers/create-team.sh
+++ git-svn-helpers/create-team.sh
@@ -3,15 +3,26 @@
 # assume this is run in the directory where the projects-git-RELEASE folder is.
 
 release_url=http://jamesbuntu.corelogic.local/gitweb-release/
+release_git_url=git://jamesbuntu.corelogic.local/projects-git-RELEASE-pub
 base_path=/home/james
 private=projects-git-TEAM
 public=projects-git-TEAM-pub
 
-# String to look for
-# <td><a class="list" href="/gitweb-release/?p=corelogic-checkserializable.git;a=summary">corelogic-checkserializable.git</a></td>
+# create folders
+mkdir $base_path/$private
+mkdir $base_path/$public
 
-curl -s $release_url | grep '<td><a class="list" href="/gitweb-release/?p=corelogic-checkserializable.git;a=summary">corelogic-checkserializable.git</a></td>'
+curl -s $release_url | sed 's/.*\(corelogic-[a-z0-9\-]*\).*/\1/g' | grep corelogic | grep -v jamesbuntu | sort -u > /tmp/teamrepolist
 
+while read line
+do
+	echo $line
+	echo $base_path/$private
+	cd $base_path/$private
 
+	git clone $release_git_url/$line.git
 
+done < /tmp/teamrepolist
 
+# clean up
+rm /tmp/teamrepolist
