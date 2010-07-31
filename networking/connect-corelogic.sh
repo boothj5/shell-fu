8eb6d4d75c9576473ec9fd4874eff65a419ee15c
James Booth <boothj5@gmail.com>
2010-07-31 16:47:55 +0100

    Single route command
    Route everything on 192.168.0.* through corelogic vpn tunnel

--- networking/connect-corelogic.sh
+++ networking/connect-corelogic.sh
@@ -10,12 +10,9 @@ echo
 # connect to vpn
 vpnc --enable-1des corelogic.conf
 
-# route to servers
-route add -net 192.168.0.29 netmask 255.255.255.255 tun0 # hudson.corelogic.local
-route add -net 192.168.0.90 netmask 255.255.255.255 tun0 # james-pc.corelogic.local
-route add -net 192.168.0.10 netmask 255.255.255.255 tun0 # svn.corelogic.local
-route add -net 192.168.0.26 netmask 255.255.255.255 tun0 # framework2.corelogic.local
+# route for corelogic machines 192.168.0.*
+route add -net 192.168.0.0 netmask 255.255.255.0 tun0
 
 # mount filesystems
-mount -t cifs -o username=james.booth,password=$password,uid=james //192.168.0.26/Share /home/james/mnt/framework2/
+mount -t cifs -o username=james.booth,password=$password,uid=james //framework2.corelogic.local/Share /home/james/mnt/framework2/
 
