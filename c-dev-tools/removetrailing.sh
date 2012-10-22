76db7872f0935170d71487456afd91e2558170f4
James Booth <boothj5@gmail.com>
2012-10-22 21:59:20 +0100

    Added src and tests folder to remove trailing script

--- c-dev-tools/removetrailing.sh
+++ c-dev-tools/removetrailing.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-for f in *.c *.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/*.c src/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in tests/*.c tests/*.h; do sed -i 's/[ \t]*$//' $f; done
