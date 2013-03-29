a3b8565f3d7250db27ce3ac56355aca22f66076d
James Booth <boothj5@gmail.com>
2013-03-29 23:50:05 +0000

    Updated removetrailing

--- c-dev-tools/removetrailing.sh
+++ c-dev-tools/removetrailing.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 for f in src/*.c src/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/xmpp/*.c src/xmpp/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/ui/*.c src/ui/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/tools/*.c src/tools/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/command/*.c src/command/*.h; do sed -i 's/[ \t]*$//' $f; done
+for f in src/config/*.c src/config/*.h; do sed -i 's/[ \t]*$//' $f; done
 for f in tests/*.c tests/*.h; do sed -i 's/[ \t]*$//' $f; done
